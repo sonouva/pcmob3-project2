bbbf668e657e329d14dfecf466e34a10c5b4f5b8
junjie <jun_jie1994@hotmail.com>
2020-12-08 11:34:37 +0800

    reset color

--- App.js
+++ App.js
@@ -1,94 +1,73 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { FlatList } from "react-native-gesture-handler";
 
-function HomeScreen({ navigation }) {
- const [colorArray, setColorArray] = useState([]);
+function HomeScreen() {
+  const [colorArray, setColorArray] = useState([]);
 
- function renderItem({ item }) {
-   return (
-     <TouchableOpacity
-       onPress={() => navigation.navigate("DetailsScreen", { ...item })}
-     >
-       <BlockRGB red={item.red} green={item.green} blue={item.blue} />
-     </TouchableOpacity>
-   );
- }
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
 
- function addColor() {
-   setColorArray([
-     ...colorArray,
-     {
-       red: Math.floor(Math.random() * 256),
-       green: Math.floor(Math.random() * 256),
-       blue: Math.floor(Math.random() * 256),
-       id: `${colorArray.length}`,
-     },
-   ]);
- }
+  function addColor() {
+    let newColor = {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: colorArray.length.toString(),
+    };
+    setColorArray([...colorArray, newColor]);
+  }
 
- return (
-   <View style={styles.container}>
-     <TouchableOpacity
-       style={{ height: 40, justifyContent: "center" }}
-       onPress={addColor}
-     >
-       <Text style={{ color: "red" }}>Add colour</Text>
-     </TouchableOpacity>
-     <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
-   </View>
- );
-}
+  function resetColor() {
+    setColorArray([]);
+  }
 
-function DetailsScreen({ route }) {
- // Destructure this object so we don't have to type route.params.red etc
- const { red, green, blue } = route.params;
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
+        <Text style={{ color: "blue" }}>Add Colour</Text>
+      </TouchableOpacity>
 
- return (
-   <View
-     style={[
-       styles.container,
-       { backgroundColor: `rgb(${red}, ${green}, ${blue})` },
-     ]}
-   >
-     <View style={{ padding: 30 }}>
-       <Text style={styles.detailText}>Red: {red}</Text>
-       <Text style={styles.detailText}>Green: {green}</Text>
-       <Text style={styles.detailText}>Blue: {blue}</Text>
-     </View>
-   </View>
- );
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={resetColor}
+      >
+        <Text style={{ color: "red" }}>Reset Colour</Text>
+      </TouchableOpacity>
+
+      <FlatList
+        style={{ width: "100%" }}
+        data={colorArray}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 }
 
 const Stack = createStackNavigator();
 
 export default function App() {
- return (
-   <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen name="Colour List" component={HomeScreen} />
-       <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Colour List" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#fff",
-   alignItems: "center",
-   justifyContent: "center",
- },
- list: {
-   width: "100%",
- },
- detailText: {
-   fontSize: 24,
-   marginBottom: 20,
- },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
 });
